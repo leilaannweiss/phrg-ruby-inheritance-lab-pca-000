2217b0fff49e0d492572eeedecff9c8c539b3189
Leila Weiss <leila.weiss@powerhrg.com>
2019-03-28 09:59:42 -0400

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,13 @@
+class User
+  attr_accessor :first_name, :last_name
+  def initialize
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(first_name, last_name)
+    @name = first_name
+    @name = last_name
+  end
+end
